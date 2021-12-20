e67a854739cf60c6240cd13353eda66872ee3086
Arpit <arpitmh23@gmail.com>
2021-12-20 23:05:24 +0530

    Checking swap condition between numbers

---
+++ SwapTwoNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class SwapTwoNumber {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println("Numbers before swaping");
+		System.out.println(num1);
+		System.out.println(num2);
+
+		int swap1 = num2;
+		int swap2 = num1;
+
+		System.out.println("Numbers after swaping");
+                System.out.println(swap1);
+                System.out.println(swap2);
+	}
+}
