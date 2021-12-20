79d913523106ec6fe3511676b298e45d306e4de9
Arpit <arpitmh23@gmail.com>
2021-12-20 22:46:04 +0530

    Implementing Changes In value of power of two

---
+++ PowerOfTwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class PowerOfTwo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter value of n");
+		int n = sc.nextInt();
+		if(n>0&&n<31){
+			for(int i=1;i<=n;i++){
+				System.out.println(i+" "+Math.pow(2,i));
+			}
+		}
+		else
+		{
+			System.out.println("Exceed max value, please enter lower value");
+		}
+	}
+
+}
